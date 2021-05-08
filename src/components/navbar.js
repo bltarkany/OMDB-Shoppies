d76988eac897e46b3851363ffea39257d513ec0f
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-05-08 12:46:00 -0700

    updated nabar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,2 +1,26 @@
-// navbar creation 
-import React, {useState, useEffect} from 'react';
+// navbar creation
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  logo: {
+    textAlign: 'center',
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  return (
+    <header className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} className={classes.logo}>
+          <h1>Welcome to the Shoppies</h1>
+        </Grid>
+      </Grid>
+    </header>
+  );
+}
