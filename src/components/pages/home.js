e495cf2d159631d4b07f0d114ef0e2a22913ac41
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-05-09 02:16:56 -0700

    updated home page with handleclick and handleDelete functions, capitalization for formatting, and mapping for both nominations cards

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -7,6 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import API from '../../utils/api';
 import Progress from '../progress';
 import Movie from '../card';
+import Nomination from '../nomination';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,26 +17,67 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexWrap: 'wrap',
   },
+  title: {
+    color: '#e32402',
+  },
 }));
 
 export default function Home() {
   const classes = useStyles();
   const [search, setSearch] = useState('');
   const [searching, setSearching] = useState(false);
-  const [movie, setMovie] = useState({});
-  const [result, setResult] = useState(false);
+  //   const [movie, setMovie] = useState({});
+  const [result, setResult] = useState([]);
+  const [picture, setPicture] = useState([]);
+  const [score, setScore] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setResult(true);
     setSearching(true);
-    console.log(search);
     const data = await API.searchMovie(search);
-    console.log(data);
-    setMovie(data.data);
+    console.log(data.data.Search);
+    setResult(data.data.Search);
     setSearching(false);
 
     document.getElementById('outlined-full-width').value = '';
+  };
+
+  const handleClick = (id, category) => {
+    console.log(id, category);
+    let movie = result.filter((x) => x.imdbID === id);
+    console.log(movie);
+    switch (category) {
+      case 'picture':
+        setPicture([movie[0], ...picture]);
+        break;
+      case 'score':
+        setScore([movie[0], ...score]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleDelete = (id, category) => {
+    console.log(id, category);
+    let filtered;
+    switch (category) {
+      case 'picture':
+        filtered = picture.filter((x) => x.imdbID !== id);
+        setPicture(filtered);
+        break;
+      case 'score':
+        filtered = score.filter((x) => x.imdbID !== id);
+        setScore(filtered);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
   };
 
   return (
@@ -67,29 +109,56 @@ export default function Home() {
       </Grid>
       <Grid container spacing={8} justify='center'>
         <Grid item xs={10} md={8}>
-          {!result ? (
+          {!result.length ? (
             <h3>No current results.</h3>
           ) : searching ? (
             <Progress />
           ) : (
-            <Movie
-              year={movie.Year}
-              rated={movie.Rated}
-              title={movie.Title}
-              actors={movie.Actors}
-              plot={movie.Plot}
-            />
+            <Grid container spacing={2} justify='center'>
+              {result.map((res) => (
+                <Grid item xs={12} sm={6}>
+                  <Movie
+                    key={res.imdbID}
+                    id={res.imdbID}
+                    year={res.Year}
+                    type={res.Type.capitalize()}
+                    title={res.Title}
+                    handleClick={handleClick}
+                  />
+                </Grid>
+              ))}
+            </Grid>
           )}
         </Grid>
       </Grid>
-      
+
       <Grid container spacing={8} justify='center'>
         <Grid item xs={10} md={8}>
-            <br />
-          <Typography variant='h4' component='h2' gutterBottom>
+          <br />
+          <br />
+          <Typography
+            variant='h4'
+            component='h2'
+            gutterBottom
+            className={classes.title}>
             And the award goes too...
           </Typography>
-          
+          <Grid container spacing={8}>
+            <Grid item xs={12} sm={6}>
+              <Nomination
+                results={picture}
+                handleDelete={handleDelete}
+                category='picture'
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Nomination
+                results={score}
+                handleDelete={handleDelete}
+                category='score'
+              />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
