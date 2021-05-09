25a60a5198bf6f2e294bab278263ff608ef9f579
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-05-09 11:58:14 -0700

    removed warnings for build

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -86,6 +86,7 @@ export default function Home() {
     setResult([]);
   };
 
+  // eslint-disable-next-line no-extend-native
   String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
   };
